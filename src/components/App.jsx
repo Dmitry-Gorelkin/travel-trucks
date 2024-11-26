476ed3979a5a00433e845d97ea31b25d99570fc3
Дмитрий Горелкин <gorelkind@gmail.com>
2024-11-26 16:30:39 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
 import { lazy } from 'react';
+import CardTrukFeatures from './CardTrukFeatures/CardTrukFeatures';
+import CardTrukReviews from './CardTrukReviews/CardTrukReviews';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
 const CatalogPage = lazy(() => import('../pages/CatalogPage/CatalogPage'));
 const CardTrukPage = lazy(() => import('../pages/CardTrukPage/CardTrukPage'));
-const CardTrukFeatures = lazy(() => import('./CardTrukFeatures/CardTrukFeatures'));
-const CardTrukReviews = lazy(() => import('./CardTrukReviews/CardTrukReviews'));
+// const CardTrukFeatures = lazy(() => import('./CardTrukFeatures/CardTrukFeatures'));
+// const CardTrukReviews = lazy(() => import('./CardTrukReviews/CardTrukReviews'));
 
 function App() {
   return (
